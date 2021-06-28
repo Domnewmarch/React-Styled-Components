bebeed569ea04cff2f798bb96750e94cf38c3d75
Domnewmarch <44242906+Domnewmarch@users.noreply.github.com>
2021-06-28 11:56:06 +0100

    nav bar

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar'
+import {BrowserRouter as Router} from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <Sidebar />
+      <Navbar />   
+    </Router>
+    
+  );
+}
+
+export default App;
